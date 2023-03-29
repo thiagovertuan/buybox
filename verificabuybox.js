cc9bf6ad5247a8956b4f068ab2a8bb1e35c94f9f
thiagovertuan <35606606+thiagovertuan@users.noreply.github.com>
2023-03-28 23:59:59 -0300

    Create verificabuybox.js

---
+++ verificabuybox.js
@@ -0,0 +1,19 @@
+const url = 'https://www.amazon.com/2021-Modern-Reloading-Manual-2nd/dp/B094ZKD1WN/';
+
+fetch(url)
+  .then(response => response.text())
+  .then(data => {
+    // Analisar a resposta do site da AMAZON e extrair informações de conteudo se temos buybox
+    
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+
+    const buyboxContainer = doc.querySelector('.tabular-buybox-container');
+
+    if (buyboxContainer && buyboxContainer.textContent.indexOf("Atreo") === -1) {
+      console.log("NÃO TEMOS BUYBOX");
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  });
